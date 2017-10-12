add5b2eb3b580d4240ae26a17a3235a5a4e5d89d
jalapagos <jalaj.pun@gmail.com>
2017-10-11 21:43:43 -0700

    Added keyboard dismissal

--- Chapter3/WorldTrotter/WorldTrotter/ConversionViewController.swift
+++ Chapter3/WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -11,8 +11,20 @@ import UIKit
 class ConversionViewController: UIViewController {
     
     @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
+    
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        textField.resignFirstResponder()
+    }
     
     @IBAction func fahrenheitFieldEditingChanged(textField: UITextField) {
-        celsiusLabel.text = textField.text
+        
+        if let text = textField.text, !text.isEmpty {
+            celsiusLabel.text = text
+        }
+        else {
+            celsiusLabel.text = "???"
+        }
+
     }
 }
