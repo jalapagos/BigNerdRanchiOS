c9dfc5b3a632f5f2b5b61f7488e61ad1a9fae5ca
jalapagos <jalaj.pun@gmail.com>
2018-01-04 20:52:21 -0800

    Made additions/changes to app using delegation

--- worldTrotter/worldTrotter/ConversionViewController.swift
+++ worldTrotter/worldTrotter/ConversionViewController.swift
@@ -13,12 +13,49 @@ class ConversionViewController: UIViewController {
     @IBOutlet var celsiusLabel: UILabel!
     @IBOutlet var textField : UITextField!
     
-    @IBAction func fahrenheitFieldEditingChanged(_ textField : UITextField) {
-        if let text = textField.text, !text.isEmpty {
-            celsiusLabel.text = text
+    var fahrenheitValue : Measurement<UnitTemperature>? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    
+    let numberFormatter : NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    } ()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateCelsiusLabel()
+    }
+    
+    var celsiusValue : Measurement<UnitTemperature>? {
+        if let fahrenheitValue = fahrenheitValue {
+            return fahrenheitValue.converted(to: .celsius)
+        }
+        else {
+            return nil
+        }
+    }
+    
+    func updateCelsiusLabel () {
+        if let celsiusValue = celsiusValue {
+            celsiusLabel.text = numberFormatter.string(from: NSNumber(value: celsiusValue.value))
         }
         else {
             celsiusLabel.text = "???"
+        }
+    }
+    
+    @IBAction func fahrenheitFieldEditingChanged(_ textField : UITextField) {
+        if let text = textField.text, let value = Double(text) {
+            fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
+        }
+        else {
+            fahrenheitValue = nil
         }
     }
     
