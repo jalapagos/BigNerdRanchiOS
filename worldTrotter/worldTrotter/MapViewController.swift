65b84259b88b0b1344f11c88444af7210b4c4ba5
jalapagos <jalaj.pun@gmail.com>
2018-05-01 21:52:57 -0700

    Finished chapter 5 - UITabBarController

--- worldTrotter/worldTrotter/MapViewController.swift
+++ worldTrotter/worldTrotter/MapViewController.swift
@@ -7,11 +7,14 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController : UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("MapViewController loaded its view")
+        
+        print("MapViewController loaded its view.")
     }
+    
 }
